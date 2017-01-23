4ecabe11f4cbc1edd22a616af3fb428de09353dc
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-23 11:50:56 +0000

    added transaction date to class

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,7 +1,8 @@
 class Transaction
   def initialize(value)
     @value = value
+    @date = Time.now.strftime('%d,%m,%Y')
   end
 
-  attr_reader :value
+  attr_reader :value, :date
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,10 +2,19 @@ require 'spec_helper'
 require './lib/transaction.rb'
 
 describe 'Transaction' do
-  
-  it 'expects a transaction to store a positive value on instantiation' do
+  it 'expects a transaction to be able to store a positive value on instantiation' do
     @transaction = Transaction.new(500)
     expect(@transaction.value).to eq 500
   end
 
+  it 'expects a transaction to be able to store a negative value on instantiation' do
+    @transaction = Transaction.new(-500)
+    expect(@transaction.value).to eq -500
+  end
+
+  it 'expects a tranaction to be able to store a date on instantiation' do
+    @transaction = Transaction.new(500)
+    todays_date = Time.now.strftime('%d,%m,%Y')
+    expect(@transaction.date).to eq(todays_date)
+  end
 end
