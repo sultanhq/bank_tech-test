2ab773922826d5c9737dcf04297876ee43a4fa99
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-23 12:17:11 +0000

    removed vacuous test

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -7,10 +7,6 @@ describe 'Bank' do
   end
 
   describe 'account' do
-    it 'expects an account to exist' do
-      expect(@account).to be_an_instance_of(Account)
-    end
-
     it 'expects a new acount to have a balance of zero' do
       expect(@account.balance).to eq 0
     end
