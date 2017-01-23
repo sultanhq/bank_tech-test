b69a2f48d9bf86b1f2bb31af190aefd5f61377fb
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-23 09:58:56 +0000

    basic user instance passing

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,7 @@
 class Account
 
+  def balance
+    return 0
+  end
+
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -7,4 +7,9 @@ describe 'account' do
     expect(account).to be_an_instance_of(Account)
   end
 
+  it 'expects a new acount to have a balance of zero' do
+    account = Account.new
+    expect(account.balance).to eq 0
+  end
+
 end
